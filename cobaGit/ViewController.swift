383b500712f1081e2885160b0d490f959bcec448
Marcia <ciananda7@gmail.com>
2019-05-08 11:21:06 +0700

    Initial Commit

---
+++ cobaGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cobaGit
+//
+//  Created by Marcia Kamila Rosananda on 08/05/19.
+//  Copyright © 2019 Marcia Kamila Rosananda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
