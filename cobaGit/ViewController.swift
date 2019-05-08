2a635af628132f9b233780aa26849df0e991d4c8
Marcia <ciananda7@gmail.com>
2019-05-08 11:24:08 +0700

    Update ViewController.swift

--- cobaGit/ViewController.swift
+++ cobaGit/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let fruits = ["mango","orange","pear","pome"]
+    let country = ["ind","eng", "US", "kkk"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
